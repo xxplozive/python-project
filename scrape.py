1c3ecce37b7a7859f8b3052c3fbf11faaee15ea6
Kartik Nath <kartiknath@gmail.com>
2018-08-22 08:29:59 -0400

    Added some comments

--- scrape.py
+++ scrape.py
@@ -1,3 +1,5 @@
+#This is my first python project
+#August 20, 2018 is when I started writing this file
 from selenium import webdriver
 driver = webdriver.Chrome(executable_path='/usr/local/bin/chromedriver')
 driver.get ('http://www.theverge.com/')
